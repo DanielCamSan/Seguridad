034a9ec5993b697a4607d488d94481793c1357dd
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-11-07 17:16:47 -0400

    feat(Services): Creating IDoctorService dto

---
+++ Services/IDoctorService.cs
@@ -0,0 +1,14 @@
+﻿using Security.Models;
+using Security.Models.DTOS;
+
+namespace Security.Services
+{
+    public interface IDoctorService
+    {
+        Task<IEnumerable<Doctor>> GetAll();
+        Task<Doctor?> GetOne(Guid id);
+        Task<Doctor> Create(CreateDoctorDto dto);
+        Task<Doctor?> Update(Guid id, UpdateDoctorDto dto);
+        Task<bool> Delete(Guid id);
+    }
+}
