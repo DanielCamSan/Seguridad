6da7b63d4f52792e48aff4d20674ee9672aec7dd
shalure10lure <yessicarado@gmail.com>
2025-11-07 17:09:21 -0400

    feat: adding entity doctor and dto update and create

---
+++ Models/DTOS/UpdateDoctor.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Security.Models.DTOS
+{
+    public class UpdateDoctor
+    {
+        public string Name { get; set; }
+        public Guid HospitalId { get; set; }
+        public string Specialty { get; set; }
+    }
+}
