8ee3cefd948942b89270c09ced5ae0398bde55da
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-11-07 17:15:17 -0400

    feat(Repositories): Creating IDoctorRepsitory dto

---
+++ Repositories/IDoctorRepository.cs
@@ -0,0 +1,13 @@
+﻿using Security.Models;
+
+namespace Security.Repositories
+{
+    public interface IDoctorRepository
+    {
+        Task<IEnumerable<Doctor>> GetAll();
+        Task<Doctor?> GetOne(Guid id);
+        Task Add(Doctor doctor);
+        Task Update(Doctor doctor);
+        Task Delete(Doctor doctor);
+    }
+}
