8702f5b7c39b43eeeb78ed06911d2ac1bff3470f
Fabio Garcia Meza <fabio.garciameza@ucb.edu.bo>
2025-11-07 17:15:34 -0400

    feat(Repositories): Creating DoctorRepsitory dto

---
+++ Repositories/DoctorRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Security.Data;
+using Security.Models;
+
+namespace Security.Repositories
+{
+    public class DoctorRepository : IDoctorRepository
+    {
+        private readonly AppDbContext _db;
+        public DoctorRepository(AppDbContext db) { _db = db; }
+
+        public async Task<IEnumerable<Doctor>> GetAll() =>
+            await _db.Doctors.ToListAsync();
+
+        public Task<Doctor?> GetOne(Guid id) =>
+            _db.Doctors.FirstOrDefaultAsync(d => d.Id == id);
+
+        public async Task Add(Doctor doctor)
+        {
+            await _db.Doctors.AddAsync(doctor);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Update(Doctor doctor)
+        {
+            _db.Doctors.Update(doctor);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Delete(Doctor doctor)
+        {
+            _db.Doctors.Remove(doctor);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
