ae612e1bd61ad634a9d46e66d157ff6876d422fd
unknown <ariana.pita@ucb.edu.bo>
2025-11-09 13:04:51 -0400

    feat: Add Doctor Model

---
+++ Models/Doctor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Security.Models
+{
+    public class Doctor
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+
+        public Guid HospitalId { get; set; }
+        public Hospital Hospital { get; set; }
+    }
+}
