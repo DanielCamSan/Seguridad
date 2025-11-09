7be179ee55ca613d0a085368afcf64ccb974c493
unknown <ariana.pita@ucb.edu.bo>
2025-11-09 13:15:07 -0400

    feat: Add IDoctorService

---
+++ Services/IDoctorService.cs
@@ -0,0 +1,17 @@
+﻿using Security.Models;
+using Security.Models.DTOS;
+
+namespace Security.Services
+{
+    public interface IDoctorService
+    {
+        Task<IEnumerable<Doctor>> GetAllDoctors();
+        Task<Doctor?> GetDoctorById(Guid id);
+        Task<Doctor> CreateDoctor(CreateDoctorDto dto);
+        Task<Doctor> UpdateDoctor(UpdateDoctorDto dto, Guid id);
+        Task DeleteDoctor(Guid id);
+    }
+
+    public record CreateDoctorDto(string Name, string Specialty, Guid HospitalId);
+    public record UpdateDoctorDto(string Name, string Specialty, Guid HospitalId);
+}
