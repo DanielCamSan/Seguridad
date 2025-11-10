2e55de02a6a1c880c54c2a89e616a7abad3374af
Adrián Mercado <adrianmercado782@gmail.com>
2025-11-10 15:36:14 -0400

    Add Doctor entity with relation to Hospital

---
+++ Models/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Security.Models
+{
+    public class Doctor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+
+        [Required, StringLength(100)]
+        public string Specialty { get; set; }
+
+        [ForeignKey("Hospital")]
+        public int HospitalId { get; set; }
+
+        public Hospital Hospital { get; set; }
+    }
+}
