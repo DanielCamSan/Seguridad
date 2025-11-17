acac151f1f61d28e57bb53c32b8d63b8105cf280
Luiscuac <luiguiandre2208@gmail.com>
2025-11-17 16:11:45 -0400

    .env

--- Program.cs
+++ Program.cs
@@ -6,9 +6,14 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
-
+using DotNetEnv;
 var builder = WebApplication.CreateBuilder(args);
-
+//env
+var port = Environment.GetEnvironmentVariable("PORT");
+if (!string.IsNullOrEmpty(port))
+{
+    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+}
 // Add services to the container.
 
 //builder.Services.AddControllers();//dependenci ciclica
@@ -21,6 +26,15 @@ builder.Services.AddControllers()
 //solucion a la dependencia cilcica
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+////////7
+///
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("AllowAll", p => p
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});////////////////
 var jwt = builder.Configuration.GetSection("Jwt");
 var keyBytes = Encoding.UTF8.GetBytes(jwt["Key"]!);
 
@@ -45,6 +59,15 @@ builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("AdminOnly", p => p.RequireRole("Admin"));
 });
+///////////
+var dbName = Environment.GetEnvironmentVariable("POSTGRES_DB");
+var dbUser = Environment.GetEnvironmentVariable("POSTGRES_USER");
+var dbPass = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+var connectionString = $"Host=localhost;Database={dbName};Username={dbUser};Password={dbPass}";
+///////////
+
+
+
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 builder.Services.AddScoped<IHospitalRepository, HospitalRepository>();
@@ -53,6 +76,10 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
 builder.Services.AddScoped<IDoctorService, DoctorService>();
+
+Console.WriteLine($"JWT_ISSUER: {jwtIssuer}");
+Console.WriteLine($"DB_USER:{dbUser}");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
