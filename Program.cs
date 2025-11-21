38c45a49e3c8207e79c8ebc1bb5a776f37e43d3d
Luiscuac <luiguiandre2208@gmail.com>
2025-11-21 00:58:45 -0400

    progam cs

--- Program.cs
+++ Program.cs
@@ -7,15 +7,14 @@ using Security.Data;
 using Security.Repositories;
 using Security.Services;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.RateLimiting;
+using Microsoft.OpenApi.Models;
 
 //dotnet add package DotNetEnv
 
 
 var builder = WebApplication.CreateBuilder(args);
 Env.Load();
-
+//PUERTO
 var port = Environment.GetEnvironmentVariable("PORT");
 if (!string.IsNullOrEmpty(port))
 {
@@ -24,7 +23,39 @@ if (!string.IsNullOrEmpty(port))
 
 builder.Services.AddControllers();
 
-builder.Services.AddOpenApi();
+//SWAGGER -------------
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Security API", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+
+//CORS-------------
 builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("AllowAll", p => p
@@ -32,7 +63,7 @@ builder.Services.AddCors(opt =>
         .AllowAnyHeader()
         .AllowAnyMethod());
 });
-
+//RATELIMITER-------------
 builder.Services.AddRateLimiter(options =>
 {
     options.RejectionStatusCode = 429;
@@ -45,7 +76,7 @@ builder.Services.AddRateLimiter(options =>
     });
 });
 
-
+//JWT AUTHENTICATION-------------
 var jwtKey = Environment.GetEnvironmentVariable("JWT_KEY");
 var jwtIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER");
 var jwtAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE");
@@ -68,12 +99,16 @@ builder.Services
             ClockSkew = TimeSpan.Zero
         };
     });
+
+
+//AUTHORIZATION POLICIES-------------
 builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("AdminOnly", p => p.RequireRole("Admin"));
 });
 var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
 
+//BASE DE DATOS-------------
 if (string.IsNullOrEmpty(connectionString))
 {
     // fallback local si quieres
@@ -85,6 +120,7 @@ if (string.IsNullOrEmpty(connectionString))
 
     connectionString = $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPass}";
 }
+//DI DEPENDENCY INJECTION-------------
 builder.Services.AddDbContext<AppDbContext>(opt =>
 opt.UseNpgsql(connectionString));
 builder.Services.AddScoped<IHospitalRepository, HospitalRepository>();
@@ -99,8 +135,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.UseHttpsRedirection();
+   
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 app.UseAuthentication();
 app.UseAuthorization();
